bd454b2e16998b4037076c65985e74b764fde414
Alejandro Prado <alejandro.prado@densitylabs.io>
2019-05-02 12:30:48 -0500

    [DL-41] Fix issue on IE 11

--- src/components/sliders/home-slider-background.js
+++ src/components/sliders/home-slider-background.js
@@ -1,48 +1,59 @@
 import React from "react";
 import { css } from 'emotion'
-import videoPoster from '../../../static/images/software-density-labs.jpg';
-
+import videoPoster from '../../../static/images/software-density-labs.jpg'
+import VideoCover from 'react-video-cover'
 const HomeSliderBackground = ({
   isVideo = false,
   src,
   alt,
 }) => (
   isVideo ?
-  (
-    <video
-      src={src}
-      autoPlay
-      muted
-      loop
-      poster= {videoPoster}
+  ( <div
       css={css`
-        height: 100%;
+        height: 360px;
         width: 100%;
+        overflow: hidden;
+        @media (min-width: 760px) {
+          height: 600px;
+        }
+      `} 
+    >
+      <VideoCover
+      videoOptions={
+        { src: src,
+          autoPlay: true,
+          muted: true ,
+          loop:true ,
+          poster: videoPoster
+        }
+      }
+      css={css`
         object-fit: cover;
         z-index: -100;
         background-size: cover;
-        overflow: hidden;
         max-height: 600px;
         filter: brightness(0.5);
         -webkit-filter: brightness(0.5);
-        min-height: 360px;
-      `}>
-    </video>
-  )
+          `}
+      >
+    </VideoCover>
+  </div>
+  )  
   :(
     <img src={src}
       alt={alt}
       css={css`
-        height: 100%;
+        height: 360px;
         width: 100%;
         object-fit: cover;
         z-index: -100;
         background-size: cover;
         overflow: hidden;
-        max-height: 600px;
         filter: brightness(0.5);
         -webkit-filter: brightness(0.5);
-        min-height: 360px;
+        @media (min-width: 760px) {
+          height: 600px;
+        };
       `}
     />
   )
