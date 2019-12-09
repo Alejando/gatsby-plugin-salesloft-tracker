419289a24c3655fc0bf6c503b7936ef777f23e8f
Yan Carlos Marín Osorio <yancarlosmarinosorio@gmail.com>
2019-12-09 17:54:59 -0500

    [DL-85] Fixing the summary of a blogpost when a filter is applied to blog page (#124)

--- src/templates/blog-tags.js
+++ src/templates/blog-tags.js
@@ -68,6 +68,8 @@ export const pageQuery = graphql`
             }
             tags
             author
+            description
+            social_summary
           }
           excerpt(pruneLength:250,format:HTML)
         }
