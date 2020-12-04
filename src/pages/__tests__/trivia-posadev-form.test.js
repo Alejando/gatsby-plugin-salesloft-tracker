33cefbad3357348d33aacfdeec4550ac7093984e
Victor Diaz <vicdiaz@Victors-MacBook-Pro.local>
2020-12-04 10:58:08 -0700

    unit test for id generator

--- src/pages/__tests__/trivia-posadev-form.test.js
+++ src/pages/__tests__/trivia-posadev-form.test.js
@@ -1,3 +1,13 @@
+import { generateRandomCode } from '../../components/forms/posadev-form'
+
 describe('trivia-posadev-form', () => {
-  it('foo', () =>expect(true).toBe(true))
+  it('code should have same prefix', () => {
+    const code =  generateRandomCode('prefix')
+    expect(code).toContain('prefix')
+  })
+  it('code should be different', () => {
+    const code1 = generateRandomCode('prefix')
+    const code2 = generateRandomCode('prefix')
+    expect(code1).not.toBe(code2)
+  })
 })
