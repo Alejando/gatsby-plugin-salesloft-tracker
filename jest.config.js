eac7cd750090d74b40b35eafccae9530edb93b2e
Victor Diaz <vicdiaz@Victors-MacBook-Pro.local>
2020-12-04 10:51:15 -0700

    test setup

--- jest.config.js
+++ jest.config.js
@@ -1,37 +1,16 @@
 module.exports = {
-  collectCoverageFrom: [
-    './organisms/**/*.{js}',
-    './pages/**/*.{js}',
-    './utils/**/*.{js}',
-    '!**/__tests__/**',
-    '!**/*.{test,spec}.js'
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100
-    }
+  transform: {
+    "^.+\\.jsx?$": `<rootDir>/jest-preprocess.js`,
   },
   moduleNameMapper: {
-    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js'
+    ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
+    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
   },
-  setupFiles: [],
-  setupFilesAfterEnv: [
-    // '<rootDir>/setupAfterEnv.js'
-  ],
-  testMatch: [
-    '**/?(*.)+(spec|test).js'
-  ],
-  testPathIgnorePatterns: [
-    '/.next/',
-    '/node_modules/',
-    '/tests/',
-    '/coverage/'
-  ],
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest'
-  }
+  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
+  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+  globals: {
+    __PATH_PREFIX__: ``,
+  },
+  testURL: `http://localhost`,
+  setupFiles: [`<rootDir>/loadershim.js`],
 }
